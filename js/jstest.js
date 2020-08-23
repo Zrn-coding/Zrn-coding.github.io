973853215b7f235f05670967a5ed156b993ce3db
Zrn-coding <zxc0978827909@gmail.com>
2020-08-23 15:33:17 +0800

    Site updated: 2020-08-23 15:33:17

--- js/jstest.js
+++ js/jstest.js
@@ -1,5 +1,5 @@
 /*添加图片top*/
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://i.imgur.com/CQimXEN.gif' title='回到顶部' >";
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
 /*添加到返回顶部按钮下*/
 document.getElementById("go-up").innerHTML += top_up;
 /* 手机客户端导航栏默认隐藏 */
@@ -76,5 +76,5 @@ function qipao() {
 qipao();
 
 $(document).ready(function(e) {
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 葉致宏');
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Zrn(葉致宏)');
 });
